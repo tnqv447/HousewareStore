24a9d802023287473af4a7009c9921dbbc2a321c
hieukut456 <trunghieu9946@gmail.com>
2020-06-10 01:21:21 +0700

    a

--- src/ItemApi/Data/Repos/ItemRepos/IITemRepository.cs
+++ src/ItemApi/Data/Repos/ItemRepos/IITemRepository.cs
@@ -4,17 +4,9 @@ using System.Threading.Tasks;
 using ItemApi.DTOs;
 using ItemApi.Models;
 
-<<<<<<< HEAD
 namespace ItemApi.Data.Repos {
     public interface IItemRepository : IRepository<Item> {
         Task<IEnumerable<ItemDTO>> GetItemsBySearch (string category = null, string searchString = null,string sortOrder = " ", DbStatus dbStatus = DbStatus.All);
-=======
-namespace ItemApi.Data.Repos
-{
-    public interface IItemRepository : IRepository<Item>
-    {
-        Task<IEnumerable<ItemDTO>> GetItemsBySearch(string category = null, string searchString = null, DbStatus dbStatus = DbStatus.All);
->>>>>>> 2065de25e9aae98f39adc274249eeaa7326a4ab0
         //Task<IEnumerable<ItemDTO>> GetItemsBySearch (List<Item> items, string category = null, string searchString = null);
         bool ItemExists(int id);
         Task Activate(int id);
