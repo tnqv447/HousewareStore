ac9838d59a83543e0fde1b9bb51a3335a63348b6
TruongGiangVu <45998623+TruongGiangVu@users.noreply.github.com>
2020-06-10 11:57:43 +0700

    fix failed

--- src/MvcClient/Controllers/ItemController.cs
+++ src/MvcClient/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,8 @@ using MvcClient.Authorization;
 using MvcClient.Models;
 using MvcClient.Services;
 using MvcClient.ViewModels;
+using System.IO;
+using System.Drawing;
 
 namespace MvcClient.Controllers
 {
@@ -62,21 +65,38 @@ namespace MvcClient.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Item item)
         {
-            if (ModelState.IsValid)
-            {
-                item.OwnerId = _identityService.Get(User).Id;
+            Console.WriteLine("hohoho " + item.PictureUrl); // cái này console hiện ở đâu vậy  cái này có mỗi cái tên image, thì đúng r input nó láy tên image thôi, xong nó tải file ảnh về thư mục của mình mà, tải sao??, thì mình set location đẻ save
+            string fullPath = Path.GetFullPath(item.PictureUrl); //lấy tuyệt đối, cơ mà ko hiểu sao tuyêt đối ở đây lại là từ folder của mình
+            //ok nha tự làm nha, mà search mvc hoặc .net core nha, tùy thô cái warm là do t ẩn mấy cái hàm create để kt nó có save file trước
+            Console.WriteLine("hohoho " + fullPath);// C:\Users\Administrator\Desktop\HousewareStore\src\MvcClient\background.jpg
+            // full pat này bị sai
+            // string folderPath = Server.MapPath("~/img/product/");
+            string folderPath = "~/img/product/";
+            string imagePath = folderPath + item.PictureUrl;
 
-                var isAuthorize = await _authorizationService.AuthorizeAsync(User, item, ItemOperations.Create);
-                if (!isAuthorize.Succeeded)
-                {
-                    return Forbid();
-                }
+            Image originalImage = Image.FromFile(item.PictureUrl);// can full path
+            // string filePath = AppDomain.CurrentDomain.BaseDirectory + savedName;
+            originalImage.Save(imagePath, System.Drawing.Imaging.ImageFormat.Jpeg);// save
 
-                await _itemService.CreateItem(item);
+            // MemoryStream ms = new MemoryStream(data);
+            // System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+            // img.Save(imagePath, System.Drawing.Imaging.ImageFormat.Jpeg);
 
-                return RedirectToAction(nameof(Index));
-            }
+            // if (ModelState.IsValid)
+            // {
+            //     item.OwnerId = _identityService.Get(User).Id;
 
+            //     var isAuthorize = await _authorizationService.AuthorizeAsync(User, item, ItemOperations.Create);
+            //     if (!isAuthorize.Succeeded)
+            //     {
+            //         return Forbid();
+            //     }
+
+            //     await _itemService.CreateItem(item);
+
+            //     return RedirectToAction(nameof(Index)); đang fix ma đang fix cái này mà nên sao t pull đc, chả lẽ muốn gửi bug lên github, cái bug của m chỉ khi nào t xài thôi, chứ có ảnh hưởng gì t đâu thì xong t sẽ pull sau, đéo 
+            // }
+            //t mới làm ajax + pagination bao ngon, pull cái xài thử m, khi t fix xong cái này đã, chứ đal
             return View();
         }
 
